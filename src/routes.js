23c429fb56b2c1e2031cb7b439a6ee505074c4aa
Darius Oh <ohsterx@gmail.com>
2021-03-20 21:59:15 +0800

    Add rewards page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import SignUp from './components/SignUp.vue'
 import Login from './components/Login.vue'
+import Reward from './components/Reward.vue'
+
 export default[
     { path: '/signup', component: SignUp},
-    { path: '/login', component: Login}
+    { path: '/login', component: Login},
+    { path: '/reward', component: Reward}
 ]
