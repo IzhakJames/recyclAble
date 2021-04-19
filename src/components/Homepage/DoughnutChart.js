2a7d4e2ca0826cb3e6a776cf308a2cf1c5fe6976
Mayve Chua <mayvechua@gmail.com>
2021-04-19 10:57:38 +0800

    change default of doughnut chart

--- src/components/Homepage/DoughnutChart.js
+++ src/components/Homepage/DoughnutChart.js
@@ -7,33 +7,38 @@ export default {
   extends: Doughnut,
   data: function () {
     return {
-        user:0,
+        user:[],
         datacollection: {
             labels: ["Your Contributions","Other Users' Contributions"],
             datasets: [{
                 label: "%",
                 backgroundColor: ["#3e95cd", "#8e5ea2"],
-                data: [],
+                data: [100],
               }]
         },
         options: {
             responsive: true,
             maintainAspectRatio: false,
+            legend : {
+              display: true,
+          }
            
 
         }
     }
   },
   methods:{
+ 
     fetchItems:function(){
-
         firebase.auth().onAuthStateChanged(() => {
        var uid = firebase.auth().currentUser.uid;
        database.collection('Users').doc(uid).get().then((doc) => {
-        this.user = doc.data().recyclingTripCounter
-        this.datacollection.datasets[0].data.push(this.user)
+        var userdata= doc.data().recyclingTripCounter
+        this.datacollection.datasets[0].data = [userdata]
+  
         database.collection('Users').doc('Admin').get().then((doc2) => {
-        this.datacollection.datasets[0].data.push(doc2.data().TotalCounter - this.user)
+          this.datacollection.datasets[0].data.push(doc2.data().TotalCounter - userdata)
+
         });    
     })})
        
@@ -43,9 +48,9 @@ export default {
   },
   mounted () {
     this.renderChart(this.datacollection, this.options)
-  },
-  created () {
-    this.fetchItems()
+  },  created() {
+    this.fetchItems();
   }
+ 
 }
 
